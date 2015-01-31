04d3ccd042be9b603f5094b3a6ad6f438d8876a5
Kevin Chan <chessmasterhong@yahoo.com>
2015-01-30 21:16:37 -0500

    Moved asset loading to boot scene. Added scene change on asset load completion.

--- src/game/config.js
+++ src/game/config.js
@@ -1,4 +1,8 @@
 pandaConfig = {
+    system: {
+        startScene: 'Boot'
+    },
+
     tileMap: {
         cacheLayersAsBitmap: true, // Cache every layer as bitmap
         cacheAsBitmap: true // Cache whole tilemap as bitmap
--- src/game/main.js
+++ src/game/main.js
@@ -7,12 +7,23 @@ game.module(
 .body(function() {
     'use strict';
 
-    game.addAsset('maps/desert.json');
-    game.addAsset('tileset/tmw_desert_spacing.png');
-
     game.createScene('Boot', {
         init: function() {
             console.log('Boot Scene');
+
+            // Add new asset to load queue
+            game.addAsset('tileset/tmw_desert_spacing.png');
+            game.addAsset('maps/desert.json');
+
+            // Initialize new loader with callback function
+            // Change game to 'Main' scene on load complete
+            var loader = new game.Loader();
+            loader.onComplete(this.loaded.bind(this));
+            loader.start();
+        },
+
+        loaded: function() {
+            game.system.setScene('Main');
         }
     });
 
