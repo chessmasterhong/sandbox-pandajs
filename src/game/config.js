19ac9e16c9c8856d3dcb9b4d0d16de1f94f1bd57
Kevin Chan <chessmasterhong@yahoo.com>
2015-01-30 19:38:53 -0500

    Added Tiled plugin. Added basic Tiled map from examples.

--- src/game/config.js
+++ src/game/config.js
@@ -1,1 +1,6 @@
-pandaConfig = {};
+pandaConfig = {
+    tileMap: {
+        cacheLayersAsBitmap: true, // Cache every layer as bitmap
+        cacheAsBitmap: true // Cache whole tilemap as bitmap
+    }
+};
--- src/game/main.js
+++ src/game/main.js
@@ -1,12 +1,21 @@
 game.module(
     'game.main'
 )
+.require(
+    'plugins.tiled.tiled'
+)
 .body(function() {
     'use strict';
+
+    game.addAsset('maps/desert.json');
+    game.addAsset('tileset/tmw_desert_spacing.png');
 
     game.createScene('Main', {
         init: function() {
             console.log('Hello World!');
+
+            var tilemap = new game.TileMap('maps/desert.json');
+            tilemap.addTo(this.stage);
         }
     });
 });
