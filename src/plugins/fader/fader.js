84533fd072f20312bd4c772b18e28564af91299d
Kevin Chan <chessmasterhong@yahoo.com>
2015-02-03 15:52:25 -0500

    Fixed JSHint warnings with fader plugin.

--- src/plugins/fader/fader.js
+++ src/plugins/fader/fader.js
@@ -2,55 +2,56 @@ game.module(
     'plugins.fader.fader'
 )
 .body(function() {
+    'use strict';
 
-game.createClass('Fader', {
-    color: 0x000000,
-    speed: 500,
-    fading: false,
+    game.createClass('Fader', {
+        color: 0x000000,
+        speed: 500,
+        fading: false,
 
-    init: function(settings) {
-        game.merge(this, settings);
+        init: function(settings) {
+            game.merge(this, settings);
 
-        this.sprite = new game.Graphics();
-        this.sprite.beginFill(this.color);
-        this.sprite.drawRect(0, 0, game.system.width, game.system.height);
-    },
+            this.sprite = new game.Graphics();
+            this.sprite.beginFill(this.color);
+            this.sprite.drawRect(0, 0, game.system.width, game.system.height);
+        },
 
-    fadeIn: function(callback) {
-        game.tweenEngine.stopTweensForObject(this.sprite);
+        fadeIn: function(callback) {
+            game.tweenEngine.stopTweensForObject(this.sprite);
 
-        this.callback = callback;
-        if (this.sprite.alpha === 0) this.sprite.alpha = 1;
-        game.system.stage.addChild(this.sprite);
+            this.callback = callback;
+            if (this.sprite.alpha === 0) this.sprite.alpha = 1;
+            game.system.stage.addChild(this.sprite);
 
-        var tween = new game.Tween(this.sprite);
-        tween.to({ alpha: 0 }, this.speed);
-        tween.onComplete(this.fadeComplete.bind(this, true));
-        tween.start();
+            var tween = new game.Tween(this.sprite);
+            tween.to({ alpha: 0 }, this.speed);
+            tween.onComplete(this.fadeComplete.bind(this, true));
+            tween.start();
 
-        this.fading = true;
-    },
+            this.fading = true;
+        },
 
-    fadeOut: function(callback) {
-        game.tweenEngine.stopTweensForObject(this.sprite);
+        fadeOut: function(callback) {
+            game.tweenEngine.stopTweensForObject(this.sprite);
 
-        this.callback = callback;
-        if (this.sprite.alpha === 1) this.sprite.alpha = 0;
-        game.system.stage.addChild(this.sprite);
+            this.callback = callback;
+            if (this.sprite.alpha === 1) this.sprite.alpha = 0;
+            game.system.stage.addChild(this.sprite);
 
-        var tween = new game.Tween(this.sprite);
-        tween.to({ alpha: 1 }, this.speed);
-        tween.onComplete(this.fadeComplete.bind(this));
-        tween.start();
+            var tween = new game.Tween(this.sprite);
+            tween.to({ alpha: 1 }, this.speed);
+            tween.onComplete(this.fadeComplete.bind(this));
+            tween.start();
 
-        this.fading = true;
-    },
+            this.fading = true;
+        },
 
-    fadeComplete: function(remove) {
-        this.fading = false;
-        if (typeof this.callback === 'function') this.callback();
-        if (remove) game.system.stage.removeChild(this.sprite);
-    }
-});
+        fadeComplete: function(remove) {
+            this.fading = false;
+            if (typeof this.callback === 'function') this.callback();
+            if (remove) game.system.stage.removeChild(this.sprite);
+        }
+    });
 
 });
